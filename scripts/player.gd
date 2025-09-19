f70ee2b20019ea848eeb2eb10bec48b77b84a2e1
mnb <fewewe96@icloud.com>
2025-09-19 21:30:06 +0300

    Revert "Merge branch 'main' of https://github.com/YuraStebel/EscapeFromKorolyov"
    This reverts commit fb2ac746684c9b149f6ca32c096420c8047eb7ff, reversing
    changes made to b9c36103f2af75481735926f6832416462654444.

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,8 +11,6 @@ const JUMP_VELOCITY = 4.5
 
 var mouse_sens: float = 0.004
 
-@onready var player_stats = $PlayerStats
-var is_alive: bool = true
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -20,7 +18,6 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		if not is_alive: return
 		rotation.y -= event.relative.x * mouse_sens
 		cam_holder.rotation.x -= event.relative.y * mouse_sens
 		cam_holder.rotation.x = clamp(cam_holder.rotation.x, -PI/2, PI/2)
@@ -31,8 +28,8 @@ func _unhandled_input(event: InputEvent) -> void:
 func use():
 	if Input.is_action_just_pressed('use'):
 		var get_collision = interact_cast.get_collider()
+		print(get_collision)
 		if get_collision and get_collision.is_in_group('pickable') and not is_item_in_hands:
-			is_item_in_hands = true
 			get_collision.reparent(hand)
 			get_collision.freeze = true
 			get_collision.position = Vector3.ZERO
@@ -49,8 +46,6 @@ func drop():
 
 
 func _physics_process(delta: float) -> void:
-	if not is_alive: return
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
