02a8849674d58388b870b6f306db4de6577de20b
mnb <fewewe96@icloud.com>
2025-09-19 22:17:29 +0300

    Added !!!!!!

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,10 @@ extends CharacterBody3D
 @onready var hand: Node3D = $CameraHolder/Camera3D/Hand
 var is_item_in_hands: bool = false
 
-const SPEED = 5.0
+var speed: float
+var walk_speed: float = 4.0
+var run_speed: float = 7.0
+
 const JUMP_VELOCITY = 4.5
 
 var mouse_sens: float = 0.004
@@ -58,17 +61,23 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_pressed('sprint'):	
+		speed = run_speed
+	else:
+		speed = walk_speed
+		
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	
 	use()
 	drop()
--- scripts/player_stats.gd
+++ scripts/player_stats.gd
@@ -3,19 +3,30 @@ extends Node
 var health_current: float
 var health_max: float = 100
 
+@onready var health_bar = $"../HUD/HBoxContainer/HPBar"
+@onready var vignette = $"../HUD/VignetteContainer"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health_current = health_max
+	
 
 func dying():
 	get_parent().is_alive = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	if health_current >= 100:
+		health_bar.visible = false
+	else: 
+		health_bar.visible = true
+	
 	health_current = max(health_current, 0)
+	
+	health_bar.value = health_current
+	
+	vignette.modulate = Color(1,1,1, 1 - health_current / 50)
 	
 	if health_current <= 0:
 		dying()
-	print(health_current)
 	
