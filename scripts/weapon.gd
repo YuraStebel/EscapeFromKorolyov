6d0b3e32df503032223dd87c37b05121e2b1ddb7
mnb <fewewe96@icloud.com>
2025-09-20 17:56:14 +0300

    updated weapon class

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -13,16 +13,14 @@ var ammo_current: int
 var fire_rate: float #ВЫСТРЕЛОВ В МИНУТУ
 var can_shoot: bool = true
 var shoot_timer: float = 0.0
-
 var last_shot_time: float = 0.0
 
-func shoot(delta):
+func shoot():
 	var current_time = Time.get_ticks_msec() / 1000.0
 	var fire_delay = 60.0 / fire_rate
 	
 	if is_in_hand and Input.is_action_pressed("attack1") and ammo_current > 0:
 		if current_time - last_shot_time >= fire_delay:
-			# Автоматический выстрел
 			shooting_audio.play()
 			var get_collision = gun_ray_cast.get_collider()
 			
@@ -33,6 +31,7 @@ func shoot(delta):
 			last_shot_time = current_time
 			print("Ammo: ", ammo_current, "/", ammo_max)
 	elif Input.is_action_just_pressed("attack1") and ammo_current == 0:
+		shooting_audio.play()
 		print("NO AMMO")
 
 func reload():
--- scripts/weapons/ka_74.gd
+++ scripts/weapons/ka_74.gd
@@ -3,6 +3,8 @@ class_name ak
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 
+@onready var KA74_shooting_audio = preload("res://sounds/weapon/KA74/KA74_shoot.ogg")
+@onready var KA74_no_ammo_audio = preload("res://sounds/weapon/KA74/KA74_no_ammo.ogg")
 
 func _ready() -> void:
 	fire_rate = 600
@@ -10,11 +12,15 @@ func _ready() -> void:
 	ammo_max = 30
 	ammo_current = ammo_max
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if is_in_hand:
 		anim_player.play("AK_HoldPose")
 		get_parent().get_node("PlayerModel").anim_player.play("Player_Hold-AK")
 	else:
 		anim_player.play("AK_T-Pose")
-	shoot(delta)
+	if ammo_current > 0 and shooting_audio.stream != KA74_shooting_audio: 
+		shooting_audio.stream = KA74_shooting_audio
+	elif ammo_current <= 0 and shooting_audio.stream != KA74_no_ammo_audio and shooting_audio.playing == false:
+		shooting_audio.stream = KA74_no_ammo_audio
+	shoot()
 	reload()
